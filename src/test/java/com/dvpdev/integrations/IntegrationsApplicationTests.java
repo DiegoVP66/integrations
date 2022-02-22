617fd19e51800c86a50573048e75b7bca804d801
DiegoVP66 <dvpdev33@gmail.com>
2022-02-21 21:19:17 -0300

    Project created, EmailDTO

---
+++ src/test/java/com/dvpdev/integrations/IntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvpdev.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
